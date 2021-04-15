07fbb775204bc9469ac681cc7663e9bd2fa317e1
Kuldeep <sdklove143@gmail.com>
2021-04-16 01:31:33 +0530

    Second Commit

---
+++ app/src/main/java/com/sdkproject/nitcsocialmedia/RegisterActivity.java
@@ -0,0 +1,20 @@
+package com.sdkproject.nitcsocialmedia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    public void GoToLoginPage(View view) {
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+    }
+}
